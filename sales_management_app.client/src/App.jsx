09a6f947ad2d6fd6c78a5f96ac574289e740f99a
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2024-01-26 02:01:35 +1300

    code optimization

--- sales_management_app.client/src/App.jsx
+++ sales_management_app.client/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/navBar';
 import Customer from './components/Customer';
